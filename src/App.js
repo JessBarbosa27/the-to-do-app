002942e9e9f03af58050ce194df663bff2986ead
Jess Barbosa <79306977+JessBarbosa27@users.noreply.github.com>
2024-06-22 00:46:39 +0100

    implemented draggable for to dos (#1)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import React from "react";
+import Draggable from "react-draggable";
+import { TaskWrapper } from "./components/TaskWrapper";
 import "./App.css";
-import {ToDoWrapper} from './components/ToDoWrapper';
 
-function App() {
-  return (
-    <div className="App">
-      <ToDoWrapper />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bounds: { left: 0, top: 0, right: 0, bottom: 0 },
+    };
+  }
+
+  componentDidMount() {
+    this.calculateBounds();
+    window.addEventListener("resize", this.calculateBounds);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.calculateBounds);
+  }
+
+  calculateBounds = () => {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    // Example: Allow the draggable element to move within the visible window
+    this.setState({
+      bounds: {
+        left: -windowWidth / 2.6,
+        top: 0,
+        right: windowWidth / 2.3 - 100,
+        bottom: windowHeight / 1.5 - 100,
+      },
+    });
+  };
+
+  render() {
+    const { bounds } = this.state;
+
+    return (
+      <Draggable bounds={bounds}>
+        <div className="App">
+          <TaskWrapper />
+        </div>
+      </Draggable>
+    );
+  }
 }
 
 export default App;
